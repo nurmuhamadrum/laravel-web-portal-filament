fbeab66bc26b24d83e9a766f8cc4e8e96dab2dcd
nurmuh <nurmuhamadrum@gmail.com>
2025-05-02 22:14:25 +0700

    first commit

---
+++ app/Filament/Resources/ArticleNewsResource.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ArticleNewsResource\Pages;
+use App\Filament\Resources\ArticleNewsResource\RelationManagers;
+use App\Models\ArticleNews;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ArticleNewsResource extends Resource
+{
+    protected static ?string $model = ArticleNews::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+
+                Forms\Components\FileUpload::make('thumbnail')
+                    ->image()
+                    ->columnSpanFull()
+                    ->required(),
+
+                Forms\Components\Select::make('category_id')
+                    ->relationship('category', 'name')
+                    ->searchable()
+                    ->preload()
+                    ->required(),
+
+                Forms\Components\Select::make('author_id')
+                    ->relationship('author', 'name')
+                    ->searchable()
+                    ->preload()
+                    ->required(),
+
+                Forms\Components\Select::make('is_featured')
+                    ->options([
+                        'featured' => 'Featured',
+                        'not_featured' => 'Not Featured',
+                    ])
+                    ->required(),
+
+                Forms\Components\RichEditor::make('content')
+                    ->required()
+                    ->columnSpanFull()
+                    ->toolbarButtons([
+                        'bold',
+                        'italic',
+                        'underline',
+                        'link',
+                        'bulletedList',
+                        'numberedList',
+                        'blockQuote',
+                        'codeBlock',
+                        'code',
+                        'clearFormatting',
+                        'undo',
+                        'redo',
+                        'fontSize',
+                        'fontFamily',
+                        'alignment',
+                        'textDirection',
+                        'removeFormat',
+                        'selectAll',
+                        'attachFiles',
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->searchable(),
+
+                // Tables\Columns\TextColumn::make('is_featured')
+                //     ->badge()
+                //     ->color(fn(string $state): string => match ($state) {
+                //         'active' => 'success',
+                //         'not_active' => 'danger',
+                //     }),
+
+                Tables\Columns\ImageColumn::make('thumbnail'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListArticleNews::route('/'),
+            'create' => Pages\CreateArticleNews::route('/create'),
+            'edit' => Pages\EditArticleNews::route('/{record}/edit'),
+        ];
+    }
+}
